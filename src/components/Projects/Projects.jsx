186ca0ae35fcbfdfa18869ce657afaf535d6cae9
shivankdixit111 <dshivank630@gmail.com>
2024-10-22 22:35:08 +0530

    updated

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 import ProjectCard from './ProjectCard'; 
 
 import simonImg from '../../assets/555px-Simon_Electronic_Game.jpg' 
-import spotifyImg from '../../assets/alexander-shatov-JlO3-oY5ZlQ-unsplash.jpg'
-import rentImg from '../../assets/rio-lecatompessy-p77qNCTFEJQ-unsplash.jpg'
+import musicSchoolImg from '../../assets/gabriel-gurrola-2UuhMZEChdc-unsplash.jpg'
+import rentImg from '../../assets/daria-nepriakhina-nwMxSzGFB68-unsplash.jpg' 
+
 
 const Projects = () => {
   return (
@@ -11,23 +12,22 @@ const Projects = () => {
         <h1 className='text-2xl md:text-4xl text-white font-bold'>Projects</h1>
         <div className='py-12 px-14 flex flex-wrap gap-5'>
             <ProjectCard title='Rent Ease' 
-              main='Developed a full-stack accommodation booking web app using HTML, CSS, JavaScript, Node.js, Express.js, and MongoDB. Implemented server-side logic, routing, data validation, and security best practices for seamless CRUD operations.'
+              main='Developed a full-stack booking app with HTML, CSS, JavaScript, Node.js, Express.js, and MongoDB, enabling secure CRUD operations and efficient server-side logic.'
               url = {rentImg} 
               demo = 'https://rentease-3.onrender.com/listings'
               srcCode = 'https://github.com/shivankdixit111/RentEase'
+            />
+            <ProjectCard title='Music School' 
+              main='Built an interactive music learning platform using Acertenity UI, Next.js, and TypeScript, offering an intuitive and responsive user experience.' 
+              url = {musicSchoolImg}
+              demo = 'https://shivankdixit111.github.io/music_school/'
+              srcCode = 'https://github.com/shivankdixit111/music_school'
             />
            <ProjectCard title='Simon Game' 
               main='Developed an interactive Simon Game using HTML, CSS, JavaScript, and Bootstrap, focusing on user engagement and responsive design' 
               url = {simonImg}
               demo = {'https://shivankdixit111.github.io/Simongame/'}
               srcCode = {'https://github.com/shivankdixit111/Simongame'}
-           />
-           
-           <ProjectCard title='Spotify Clone' 
-              main='Built a Spotify clone using HTML and CSS, replicating the core UI components and design for a seamless user experience.' 
-              url = {spotifyImg}
-              demo = 'https://shivankdixit111.github.io/Spotify_clone/'
-              srcCode = 'https://github.com/shivankdixit111/Spotify_clone'
            />
         </div>
     </div>
